d727efe87fc91faefbcba851e1d3b9a703ed7b61
Chuanwen Chen <chuanwen@gmail.com>
2017-07-24 00:20:54 -0700

    chapter 4 (machine language): implement multiply-two-numbers and fill-screen in asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,50 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+
+(LOOP)      
+      @24576  // keyboard
+      D=M
+      @PRESSED
+      D; JNE  // if key pressed, goto PRESSED (set color = -1), else set color = 0
+      @color
+      M=0
+      @ENDKEY
+      0; JMP
+(PRESSED)
+      @color
+      M=-1
+(ENDKEY)
+      @8192
+      D=A
+      @N
+      M=D // N = 256 * 512 / 16 = 8192
+      
+      @i
+      M=0 // i = 0
+(PAINT)
+      @i
+      D=M
+      @16384  // screen
+      D=A+D   // d = screen + i
+      @dest
+      M=D     // dest = screen + i
+      @color
+      D=M
+      @dest
+      A=M
+      M=D
+      
+      @i
+      M=M+1
+      @i
+      D=M
+      
+      @N
+      D=M-D   // D = N - i
+      @PAINT
+      D; JGT // if N > i, goto PAINT
+      @LOOP
+      0; JMP
+      
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -5,5 +5,55 @@
 
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-
-// Put your code here.
+// High level code:
+// set R2 = 0
+// set i = 1
+// set x = R1
+// while (true) {
+//   if (R0 & i = 1) then R2 = R2 + x
+//   i = i + i
+//   x = x + x
+//   if (i > R0) then break
+// } 
+      @2
+      M=0 // R2 = 0
+      
+      @i
+      M=1 // i = 1
+      
+      @1
+      D=M
+      @x
+      M=D // x = R1
+      
+(LOOP)      
+      @0
+      D=M
+      @i
+      A=M
+      D=D&A // D = R0 & i
+      
+      @NEXT
+      D; JEQ // if R0 & i == 0 goto NEXT
+      @x
+      D=M
+      @2
+      M=D+M  // R2 = R2 + x
+(NEXT)      
+      @i
+      D=M
+      M=D+M // i = i + i
+      
+      @x
+      D=M
+      M=D+M // x = x + x
+      
+      @i
+      D=M
+      @0
+      D=D-M
+      @LOOP
+      D; JLE // if i <= R0 goto LOOP
+(END)
+      @END
+      0; JMP
